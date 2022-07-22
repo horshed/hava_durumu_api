de935f24b420a446ab4d49a0f374b5dfaa9d6042
Sefa <sefabsbtn@gmail.com>
2022-07-22 14:09:36 +0300

    hava_durumu_api

---
+++ api2/api2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace api2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string api= "89558ddd00371adfaa8d24417e2da411";
+            string bag = "https://api.openweathermap.org/data/2.5/weather?q=Antalya&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument hava= XDocument.Load(bag);
+            var temp = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            Console.WriteLine("Antalya Hava Durumu   "+temp);
+            Console.Read();
+
+
+        }
+    }
+}
